99e134bd5abe9672c2ecc8e4c0e7806a077c0551
superstaroof <62721803+superstaroof@users.noreply.github.com>
2020-06-30 02:22:05 +0200

    game.cpp
    the game

---
+++ game.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a;
+int d;
+cout << "Destroy an Asteroid by Typing in D and Hit Enter!";
+cin >> a;
+if a = d(){d=d+1};
+cout << d;
+return 0;
+};
+
